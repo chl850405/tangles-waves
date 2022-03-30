95d695dea16105d0ca88d1bb86dcfe921587f81a
chl850405 <christimarchetti@gmail.com>
2022-03-30 10:09:51 -0400

    Navbar

---
+++ src/pages/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+
+function Welcome() {
+  return (
+   <>
+      <Navbar/>
+      </>
+  );
+}
+
+export default Welcome;

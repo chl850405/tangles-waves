260ca4e243de1bb2c76af646f0915cdb5c2681cb
chl850405 <christimarchetti@gmail.com>
2022-04-06 23:31:28 -0400

    add import js static to html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import Welcome from "./pages/Welcome"
 import './App.css';
+import "bootstrap"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div>
+     <main>
+<Welcome></Welcome>
+     </main>
     </div>
   );
 }
